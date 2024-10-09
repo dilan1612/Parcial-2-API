c64d82e566526e4d13a5b2e7c2e66d3d0543efc0
dilan1612 <93342327+dilan1612@users.noreply.github.com>
2024-10-08 19:44:55 -0500

    cambios sin guardar

--- index.js
+++ index.js
@@ -1,63 +1,16 @@
-const express = require('express');
-const fs = require('fs'); // Para leer archivos
-const path = require('path'); // Para manejar rutas
-const app = express();
+const express = require('express')
 
-// Configuración de EJS como motor de vistas
-app.set('view engine', 'ejs');
-app.use(express.static('public')); // Archivos estáticos como CSS
+const path = require('path')
 
-// Ruta principal que lee el archivo JSON
-app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, 'public','data.json');
+const app = express()
 
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo JSON', err);
-            return res.status(500).send('Error al obtener los personajes');
-        }
+const PORT = 3000
 
-        // Parsear el JSON y extraer los personajes de la propiedad 'data'
-        const jsonData = JSON.parse(data);
-        const characters = jsonData.data; // Accede al array de personajes dentro de 'data'
-        console.log(characters); // Verifica el contenido aquí
-        res.render('index', { characters });
-    });
-});
+//Middleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
+//Enrutamiento
+app.use("/", require("./routes/index"))
 
-// Ruta para buscar por ObjectId
-app.get('/character/:id', (req, res) => {
-    const filePath = path.join(__dirname, 'public','data.json');
-
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo JSON', err);
-            return res.status(500).send('Error al obtener el personaje');
-        }
-
-        const jsonData = JSON.parse(data);
-        const characters = jsonData.data; // Accede al array de personajes
-
-        // Obtener el ObjectId de la consulta
-        const characterId = req.query.id;
-
-        // Buscar el personaje por ObjectId
-        const character = characters.find(char => char._id === characterId);
-
-        if (character) {
-            res.render('character', { character }); // Renderizar la vista del personaje
-        } else {
-            res.status(404).send('Personaje no encontrado');
-        }
-    });
-});
-
-
-
-
-
-// Servidor escuchando en el puerto 3000
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
-});
+app.listen(PORT, () => console.log(`Server ready at port ${PORT}`));
---
+++ public/js/script.js
@@ -0,0 +1,193 @@
+
+function loadData() {
+  return new Promise((resolve, reject) => {
+    fetch('https://starwars-n5ec-developuptcs-projects.vercel.app/')
+      .then(result => result.json())
+      .then(result => {
+        resolve(result);
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function updateTable(data) {
+    const tableBody = document.querySelector('#table tbody');
+  
+    data.forEach(star => {
+      const row = tableBody.insertRow();
+  
+      const cellid = row.insertCell(0);
+      cellid.textContent = star._id;
+  
+      const cellName = row.insertCell(1);
+      cellName.textContent = star.name;
+
+      const cellHeight = row.insertCell(2);
+      cellHeight.textContent = star.height;
+  
+      const cellMass = row.insertCell(3);
+      cellMass.textContent = star.mass;
+
+      const cellHair_color = row.insertCell(4);
+      cellHair_color.textContent = star.hair_color;
+  
+      const cellSkin_color = row.insertCell(5);
+      cellSkin_color.textContent = star.skin_color;
+
+      const cellEye_color = row.insertCell(6);
+      cellEye_color.textContent = star.eye_color;
+
+      const cellBirth_year = row.insertCell(7);
+      cellBirth_year.textContent = star.birth_year;
+
+      const cellGender = row.insertCell(8);
+      cellGender.textContent = star.gender;
+      
+      const cellHomeworld = row.insertCell(9);
+      cellHomeworld.textContent = star.homeworld;
+
+      const cellSpecies = row.insertCell(10);
+      cellSpecies.textContent = star.species;
+
+
+    })
+}
+
+function begin() {
+    document.addEventListener('DOMContentLoaded', () => {
+        loadData()
+          .then(result => {
+            updateTable(result.data)
+          })
+          .catch(error => {
+            console.error('Error al cargar datos:', error)
+          });
+      })
+}
+
+
+function searchCharacter() {
+  const characterId = document.getElementById('characterId').value;
+
+  if (characterId) {
+    getCharacterById(characterId)
+      .then(character => {
+        if (character) {
+          displayCharacterData(character);
+        } else {
+          alert('Character not found');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  } else {
+    alert('Please enter a character ID');
+  }
+}
+
+function getCharacterById(objectId) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://starwars-n5ec-developuptcs-projects.vercel.app/${objectId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(result => result.json())
+      .then(result => {
+        resolve(result);
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function displayCharacterData(character) {
+  const tableBody = document.getElementById('characterData');
+  tableBody.innerHTML = '';
+  const row = `
+    <tr>
+      <td>${character._id}</td>
+      <td>${character.name}</td>
+      <td>${character.height}</td>
+      <td>${character.mass}</td>
+      <td>${character.hair_color}</td>
+      <td>${character.skin_color}</td>
+      <td>${character.eye_color}</td>
+      <td>${character.birth_year}</td>
+      <td>${character.gender}</td>
+      <td>${character.homeworld}</td>
+      <td>${character.species}</td>
+    </tr>
+  `;
+  tableBody.innerHTML = row;
+}
+
+
+
+
+
+function searchCharacter() {
+  const characterId = document.getElementById('characterId').value;
+
+  if (characterId) {
+    getCharacterById(characterId)
+      .then(character => {
+        console.log('Character Data:', character); 
+        if (character) {
+          displayCharacterData(character);
+        } else {
+          alert('Character not found');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  } else {
+    alert('Please enter a character ID');
+  }
+}
+
+function getCharacterById(objectId) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://starwars-n5ec-developuptcs-projects.vercel.app/${objectId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(result => result.json())
+      .then(result => {
+        resolve(result);
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function displayCharacterData(character) {
+  const tableBody = document.getElementById('characterData');
+  
+
+  tableBody.innerHTML = '';
+    const characterData = character._id ? character : character[0] || character.data || character.results;
+  const row = `
+    <tr>
+      <td>${characterData._id || 'N/A'}</td>
+      <td>${characterData.name || 'N/A'}</td>
+      <td>${characterData.height || 'N/A'}</td>
+      <td>${characterData.mass || 'N/A'}</td>
+      <td>${characterData.hair_color || 'N/A'}</td>
+      <td>${characterData.skin_color || 'N/A'}</td>
+      <td>${characterData.eye_color || 'N/A'}</td>
+      <td>${characterData.birth_year || 'N/A'}</td>
+      <td>${characterData.gender || 'N/A'}</td>
+      <td>${characterData.homeworld || 'N/A'}</td>
+      <td>${characterData.species || 'N/A'}</td>
+    </tr>
+  `;
+
+  tableBody.innerHTML = row;
+}
+
+  begin()
+  
